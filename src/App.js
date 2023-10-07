790f700178f0eb97142b3e17bc8f14d157ba376a
Tulio Salvatierra <salvacorp@gmail.com>
2023-10-07 11:52:20 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import debounce from "lodash.debounce";
 
 function App() {
+  const [user, setUser] = useState();
+  const [githubUser, setGithubUser] = useState({
+    username: "",
+    name: "",
+    followers: 0,
+  });
+
+  const InputWithDebouncedOnchange = () => {
+    const onChange = (e) => {
+      console.log("Changed value:", e.target.value);
+      setUser(e.target.value);
+    };
+
+    const debouncedOnChange = debounce(onChange, 1000);
+
+    return <input onChange={debouncedOnChange} />;
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetch(`https://api.github.com/users/${user}`);
+      const data = await res.json();
+      setGithubUser({
+        user: data.login,
+        fullname: data.name,
+        followers: data.followers,
+      });
+    };
+    fetchUser();
+  }, [user]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Debounced input</h1>
+      <InputWithDebouncedOnchange />
+      <h1>{githubUser.user}</h1>
+      <h1>{githubUser.fullname}</h1>
+      <h1>{githubUser.followers}</h1>
+    </>
   );
 }
 
